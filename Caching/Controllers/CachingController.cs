041a88f7190a9f7f9a59eb7bd1ee7c1018c3fa55
Ahmed Elsayed <fcih_ahmed@hotmail.com>
2019-10-28 13:32:09 +0200

    initial

---
+++ Caching/Controllers/CachingController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Caching.Controllers
+{
+    [Route("api/[controller]")]
+    public class CachingController : Controller
+    {
+        private readonly IMemoryCache _memoryCach;
+
+        public CachingController(IMemoryCache memoryCache)
+        {
+            _memoryCach = memoryCache;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            bool exist = _memoryCach.TryGetValue("CahchedItem", out string userName);
+            if (!exist)
+            {
+                userName = "TestCaching";
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromDays(1));
+                _memoryCach.Set("CahchedItem", userName, cacheEntryOptions);
+            }
+            return Ok(userName);
+        }
+
+
+        [HttpPost]
+        public IActionResult Post(string value)
+        {
+            _memoryCach.GetOrCreate("CahchedItem", data =>
+            {
+                data.SlidingExpiration = TimeSpan.FromDays(1);
+                return value;
+            });
+            return Created("", "");
+        }
+
+
+        [HttpDelete]
+        public void Delete(string key)
+        {
+            _memoryCach.Remove(key);
+        }
+    }
+}
